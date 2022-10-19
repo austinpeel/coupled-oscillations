a5b1eeb435a22a9b71120cd9143ce94d5ed6d4a3
Austin Peel <austinpeel@gmail.com>
2022-10-19 18:43:57 +0200

    Make sliders for simulation variables

---
+++ Assets/Utils/UI/Slider/LabeledSlider.cs
@@ -0,0 +1,30 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Slider))]
+public class LabeledSlider : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI valueTMP;
+    [SerializeField, Range(1, 3)] private int numDecimalDigits = 1;
+
+    private Slider slider;
+
+    private void OnValidate()
+    {
+        Debug.Log("Validating");
+        var rect = valueTMP.GetComponent<RectTransform>();
+        rect.sizeDelta = (30 + 10 * numDecimalDigits) * Vector2.right + rect.sizeDelta.y * Vector2.up;
+        rect.anchoredPosition = (40 + 10 * numDecimalDigits) * Vector2.right + rect.anchoredPosition.y * Vector2.up;
+    }
+
+    public void SetValue(float value)
+    {
+        string format = "0.";
+        for (int i = 0; i < numDecimalDigits; i++)
+        {
+            format += "0";
+        }
+        if (valueTMP) valueTMP.text = value.ToString(format);
+    }
+}

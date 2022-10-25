c2d9afaf4a66b3cb869747337dfda2f105dacfc7
Austin Peel <austinpeel@gmail.com>
2022-10-25 18:18:08 +0200

    Progress on theory

--- Assets/Simulation/Scripts/CoupledOscillationsSimulation.cs
+++ Assets/Simulation/Scripts/CoupledOscillationsSimulation.cs
@@ -12,12 +12,22 @@ public class CoupledOscillationsSimulation : Simulation
     [SerializeField] private Transform wall2;
 
     [Header("Parameters")]
-    [SerializeField] private double x1_init = -1;
-    [SerializeField] private double x2_init = 1;
-    [SerializeField] private double x1_ref = -2;
-    [SerializeField] private double x2_ref = 2;
-    [SerializeField] private float x1_wall = -5;
-    [SerializeField] private float x2_wall = 5;
+    [SerializeField] private double x1Init = -2;
+    [SerializeField] private double x2Init = 2;
+    [SerializeField] private double x1Ref = -2;
+    [SerializeField] private double x2Ref = 2;
+    [SerializeField] private float x1Wall = -5;
+    [SerializeField] private float x2Wall = 5;
+
+    [Header("Interactivity")]
+    [SerializeField] private bool massesAreDraggable;
+    [SerializeField] private Vector2 x1Range = Vector2.left;
+    [SerializeField] private Vector2 x2Range = Vector2.right;
+    private LayerMask massLayerMask;
+    private Plane plane;
+    private Camera mainCamera;
+    private Transform dragMass = null;
+    private Vector3 dragOffset = Vector3.zero;
 
     private double[] x;
     private double[] xdot;
@@ -25,19 +35,86 @@ public class CoupledOscillationsSimulation : Simulation
 
     private void Awake()
     {
+        // Initialize the constants matrix and compute its components
         InitializeCouplingConstants();
 
-        double x1 = x1_init - x1_ref;
-        double x2 = x2_init - x2_ref;
-        x = new double[4] { x1, x2, 0, 0 };
+        // Initialize equations of motion arrays
+        x = new double[4] { x1Init - x1Ref, x2Init - x2Ref, 0, 0 };
         double[] a = ComputeAccelerations();
         xdot = new double[4] { 0, 0, a[0], a[1] };
 
+        // Initialize object positions
         SetWallPositions();
         UpdateMassPositions();
         UpdateSpringPositions();
+
+        // For raycasting and dragging masses
+        plane = new Plane(Vector3.forward, Vector3.zero);
+        mainCamera = Camera.main;
+        massLayerMask = LayerMask.GetMask("Masses");
     }
 
+    // Handle mouse clicks and dragging masses
+    private void Update()
+    {
+        if (!massesAreDraggable) return;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitInfo;
+            float maxDistance = 2 * Mathf.Abs(mainCamera.transform.position.z);
+            if (Physics.Raycast(ray, out hitInfo, maxDistance, massLayerMask))
+            {
+                dragMass = hitInfo.transform;
+                dragOffset = hitInfo.point - dragMass.position;
+                Pause();
+            }
+        }
+
+        if (dragMass != null && Input.GetMouseButton(0))
+        {
+            Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+            float distance;
+            if (plane.Raycast(ray, out distance))
+            {
+                Vector3 position = dragMass.position;
+                position.x = ray.GetPoint(distance).x;
+                if (dragMass.name.Contains("1"))
+                {
+                    position.x = Mathf.Min(Mathf.Max(position.x, x1Range.x), x1Range.y);
+                    UpdateX(position.x - (float)x1Ref, (float)x[1]);
+                }
+                else if (dragMass.name.Contains("2"))
+                {
+                    position.x = Mathf.Min(Mathf.Max(position.x, x2Range.x), x2Range.y);
+                    UpdateX((float)x[0], position.x - (float)x2Ref);
+                }
+                dragMass.position = position;
+                UpdateSpringPositions();
+            }
+        }
+
+        if (dragMass != null && Input.GetMouseButtonUp(0))
+        {
+            if (dragMass.name.Contains("1"))
+            {
+                UpdateX(dragMass.position.x - (float)x1Ref, (float)x[1]);
+            }
+            else if (dragMass.name.Contains("2"))
+            {
+                UpdateX((float)x[0], dragMass.position.x - (float)x2Ref);
+            }
+            UpdateXDot();
+            UpdateSpringPositions();
+
+            dragMass = null;
+            dragOffset = Vector3.zero;
+            Resume();
+        }
+    }
+
+    // Solve the equations of motion
     private void FixedUpdate()
     {
         if (paused) { return; }
@@ -46,7 +123,6 @@ public class CoupledOscillationsSimulation : Simulation
         double deltaTime = Time.fixedDeltaTime / numSubsteps;
         for (int i = 0; i < numSubsteps; i++)
         {
-            // TakeEulerStep(deltaTime);
             TakeLeapfrogStep(deltaTime);
         }
 
@@ -56,40 +132,29 @@ public class CoupledOscillationsSimulation : Simulation
 
     public void UpdateMassPositions()
     {
-        if (mass1) mass1.transform.position = (float)(x1_ref + x[0]) * Vector3.right;
-        if (mass2) mass2.transform.position = (float)(x2_ref + x[1]) * Vector3.right;
+        if (mass1) mass1.transform.position = (float)(x1Ref + x[0]) * Vector3.right;
+        if (mass2) mass2.transform.position = (float)(x2Ref + x[1]) * Vector3.right;
     }
 
     private void SetWallPositions()
     {
-        if (wall1) wall1.position = x1_wall * Vector3.right;
-        if (wall2) wall2.position = x2_wall * Vector3.right;
+        if (wall1) wall1.position = x1Wall * Vector3.right;
+        if (wall2) wall2.position = x2Wall * Vector3.right;
     }
 
     public void UpdateSpringPositions()
     {
-        float x1 = (float)(x[0] + x1_ref);
-        float x2 = (float)(x[1] + x2_ref);
+        float x1 = (float)(x[0] + x1Ref);
+        float x2 = (float)(x[1] + x2Ref);
 
         float offsetWall1 = wall1 ? 0.5f * wall1.localScale.x : 0;
         float offsetWall2 = wall2 ? 0.5f * wall2.localScale.x : 0;
         float offsetMass1 = mass1 ? mass1.HalfScale : 0;
         float offsetMass2 = mass2 ? mass2.HalfScale : 0;
 
-        if (spring1)
-        {
-            spring1.SetEndpoints((x1_wall + offsetWall1) * Vector3.right, (x1 - offsetMass1) * Vector3.right);
-        }
-
-        if (spring2)
-        {
-            spring2.SetEndpoints((x1 + offsetMass1) * Vector3.right, (x2 - offsetMass2) * Vector3.right);
-        }
-
-        if (spring3)
-        {
-            spring3.SetEndpoints((x2 + offsetMass2) * Vector3.right, (x2_wall - offsetWall2) * Vector3.right);
-        }
+        if (spring1) spring1.SetEndpoints((x1Wall + offsetWall1) * Vector3.right, (x1 - offsetMass1) * Vector3.right);
+        if (spring2) spring2.SetEndpoints((x1 + offsetMass1) * Vector3.right, (x2 - offsetMass2) * Vector3.right);
+        if (spring3) spring3.SetEndpoints((x2 + offsetMass2) * Vector3.right, (x2Wall - offsetWall2) * Vector3.right);
     }
 
     private double[] ComputeAccelerations()
@@ -155,9 +220,36 @@ public class CoupledOscillationsSimulation : Simulation
         xdot[3] = a[1];
     }
 
-    public double[] GetX1X2()
+    public double[] GetX1X2(bool absolute = false)
     {
-        return new double[2] { x[0], x[1] };
+        double[] x1x2 = new double[2] { x[0], x[1] };
+        if (absolute)
+        {
+            x1x2[0] += x1Ref;
+            x1x2[1] += x2Ref;
+        }
+        return x1x2;
+    }
+
+    public float[] GetSpring1Endpoints()
+    {
+        float offsetWall1 = wall1 ? 0.5f * wall1.localScale.x : 0;
+        float offsetMass1 = mass1 ? mass1.HalfScale : 0;
+        return new float[2] { x1Wall + offsetWall1, (float)(x[0] + x1Ref) - offsetMass1 };
+    }
+
+    public float[] GetSpring2Endpoints()
+    {
+        float offsetMass1 = mass1 ? mass1.HalfScale : 0;
+        float offsetMass2 = mass2 ? mass2.HalfScale : 0;
+        return new float[2] { (float)(x[0] + x1Ref) + offsetMass1, (float)(x[1] + x2Ref) - offsetMass2 };
+    }
+
+    public float[] GetSpring3Endpoints()
+    {
+        float offsetWall2 = wall2 ? 0.5f * wall2.localScale.x : 0;
+        float offsetMass2 = mass2 ? mass2.HalfScale : 0;
+        return new float[2] { (float)(x[1] + x2Ref) + offsetMass2, x2Wall - offsetWall2 };
     }
 
     public void EnterNormalMode(bool first, float amplitude)
@@ -203,5 +295,23 @@ public class CoupledOscillationsSimulation : Simulation
         double KE = mass1.mass * x[2] * x[2] + mass2.mass * x[3] * x[3];
         double PE = spring1.k * (x[0] * x[0] + x[1] * x[1]) + spring2.k * (x[1] - x[0]) * (x[1] - x[0]);
         return 0.5 * (KE + PE);
+    }
+
+    public void Reset(bool pause)
+    {
+        // Initialize the constants matrix and compute its components
+        InitializeCouplingConstants();
+
+        // Initialize equations of motion arrays
+        x = new double[4] { x1Init - x1Ref, x2Init - x2Ref, 0, 0 };
+        double[] a = ComputeAccelerations();
+        xdot = new double[4] { 0, 0, a[0], a[1] };
+
+        // Initialize object positions
+        SetWallPositions();
+        UpdateMassPositions();
+        UpdateSpringPositions();
+
+        if (pause) Pause();
     }
 }
--- Assets/Simulation/Scripts/MassiveObject.cs
+++ Assets/Simulation/Scripts/MassiveObject.cs
@@ -5,9 +5,41 @@ public class MassiveObject : MonoBehaviour
     [Min(0)] public float mass = 1f;
     public float HalfScale { get { return 0.5f * transform.localScale.x; } }
 
+    [Header("Interactions")]
+    [SerializeField] private Texture2D hoverCursor = null;
+    [SerializeField] private Vector2 hotspot = default;
+
     public void SetMass(float mass)
     {
         transform.localScale = (1 + Mathf.Log10(Mathf.Max(0.1f, Mathf.Pow(mass, 0.33f)))) * Vector3.one;
         this.mass = mass;
+    }
+
+    private void OnMouseEnter()
+    {
+        // Display the cursor while hovering
+        if (hoverCursor != null)
+        {
+            Cursor.SetCursor(hoverCursor, hotspot, CursorMode.Auto);
+        }
+    }
+
+    private void OnMouseExit()
+    {
+        RestoreDefault();
+    }
+
+    private void RestoreDefault()
+    {
+        // Restore the default cursor
+        if (hoverCursor != null)
+        {
+            Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto);
+        }
+    }
+
+    private void OnDisable()
+    {
+        RestoreDefault();
     }
 }

2ae9023a7b4960d24b310d26cc7ad602b465f8aa
Austin Peel <austinpeel@gmail.com>
2022-11-07 19:31:21 +0100

    Add position markers and update equations

--- unity/coupled-oscillations/Assets/Activities/Activity1/XAxis.cs
+++ unity/coupled-oscillations/Assets/Activities/Activity1/XAxis.cs
@@ -5,6 +5,8 @@ public class XAxis : MonoBehaviour
     [SerializeField] private CoupledOscillationsSimulation sim;
     [SerializeField] private Transform x1;
     [SerializeField] private Transform x2;
+    [SerializeField] private Transform xCM;
+    [SerializeField] private Transform xR;
 
     private void Update()
     {
@@ -19,5 +21,7 @@ public class XAxis : MonoBehaviour
 
         if (x1) x1.localPosition = (float)x[0] * Vector3.right;
         if (x2) x2.localPosition = (float)x[1] * Vector3.right;
+        if (xCM) xCM.localPosition = 0.5f * (float)(x[0] + x[1]) * Vector3.right;
+        if (xR) xR.localPosition = 0.5f * (float)(x[1] - x[0] - 2 * sim.x0) * Vector3.right;
     }
 }

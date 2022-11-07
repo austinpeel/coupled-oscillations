361d82fa39173e99bf634b8be51764531127138d
Austin Peel <austinpeel@gmail.com>
2022-11-07 08:13:40 +0100

    Correct solution equations

--- unity/coupled-oscillations/Assets/Theory/Scripts/TheoryController.cs
+++ unity/coupled-oscillations/Assets/Theory/Scripts/TheoryController.cs
@@ -5,6 +5,8 @@ public class TheoryController : MonoBehaviour
     [SerializeField] private CoupledOscillationsSimulation sim;
     [SerializeField] private Transform x1;
     [SerializeField] private Transform x2;
+    [SerializeField] private Transform xCM;
+    [SerializeField] private Transform xR;
 
     [Header("References")]
     [SerializeField] private LengthScale lsLeftRef;
@@ -49,6 +51,8 @@ public class TheoryController : MonoBehaviour
 
         if (x1) x1.localPosition = (float)x[0] * Vector3.right;
         if (x2) x2.localPosition = (float)x[1] * Vector3.right;
+        if (xCM) xCM.localPosition = (float)(0.5 * (x[0] + x[1])) * Vector3.right;
+        if (xR) xR.localPosition = (float)(0.5 * (x[1] - x[0]) - 2) * Vector3.right;
     }
 
     private void SetReferenceLengthScales()

70979f887c134794fee0d59c4dc36d59c8956d59
Austin Peel <austinpeel@gmail.com>
2022-12-14 15:25:45 +0100

    Fix survey link rel

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,11 @@ export default function Header() {
   return (
     <div className='header'>
       <h3>An interactive simulation on coupled oscillations.</h3>
-      <a href='https://forms.gle/Dx3gPHU51c1UZT7f8' target='_blank'>
+      <a
+        href='https://forms.gle/Dx3gPHU51c1UZT7f8'
+        target='_blank'
+        rel='noreferrer'
+      >
         Help us improve!
       </a>
     </div>
